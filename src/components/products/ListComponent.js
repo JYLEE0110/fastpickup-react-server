a82b080fa2e9e8587a9fc4afd0315bc0ef773652
JY LEE <wndyd0110@gmail.com>
2024-01-18 18:24:21 +0900

    상품 레이아웃 적용

--- src/components/products/ListComponent.js
+++ src/components/products/ListComponent.js
@@ -1,37 +1,56 @@
-import { useEffect, useState } from "react"
-import { getProductList } from "../../api/productAPI"
+import { useEffect, useState } from "react";
+import { getProductList } from "../../api/productAPI";
 
 const initState = {
-    list: [],
-    endNum: 0,
-    startNum: 0,
-    nextBtn: false,
-    prevBtn: false,
-    pageNums: [],
-    page: 0,
-    size: 0,
-    requestDTO: null
-  }
+  list: [],
+  endNum: 0,
+  startNum: 0,
+  nextBtn: false,
+  prevBtn: false,
+  pageNums: [],
+  page: 0,
+  size: 0,
+  requestDTO: null,
+};
 
-const ListComponent = ({queryObj, movePage, moveRead}) => {
-    
-    const[productList, setProductList] = useState(initState)
-    
-    useEffect(() => {
+const ListComponent = ({ queryObj, movePage, moveRead }) => {
+  const [productList, setProductList] = useState(initState);
 
-        getProductList(queryObj).then((data) => {
-            setProductList(data)
-        })
-        
-    },[queryObj])
-    
-    console.log(productList)
-    
-    return ( 
-        <div>
+  useEffect(() => {
+    getProductList(queryObj).then((data) => {
+      setProductList(data);
+    });
+  }, [queryObj]);
 
-        </div>
-     );
-}
- 
+  console.log(productList);
+
+  return (
+<div>
+      <ul className="mt-3 ml-6">
+        {productList.list.map(
+          ({ pno, productName, productPrice, imgName, registDate }) => (
+            <li key={pno} className="pb-3 mb-3 border-b border-[#eee]" style={{ fontSize: "14px" }}>
+              <div className="flex">
+                <div className="w-[80px] h-[80px] overflow-hidden rounded-full">
+                  <img
+                    src={`http://172.29.99.60/product/${imgName}`}
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+                <div className="flex flex-col ml-2">
+                  <div className="text-[16px] font-medium mb-1">
+                    {productName}
+                    {/* {recStatus === 1 ? <img src={require(`../../images/product_recommend.png`)} className="inline-block w-[26px] ml-1 align-top" /> : ""} */}
+                  </div>
+                  <div className="text-[14px]">{productPrice.toLocaleString()}원</div>
+                </div>
+              </div>
+            </li>
+          )
+        )}
+      </ul>
+    </div>
+  );
+};
+
 export default ListComponent;
