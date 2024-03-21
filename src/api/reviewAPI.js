52bcf175aa64f254591c16a174992be80106b321
JY LEE <wndyd0110@gmail.com>
2024-03-21 20:30:32 +0900

    마이페이지 리뷰목록 구현 완료

--- src/api/reviewAPI.js
+++ src/api/reviewAPI.js
@@ -2,13 +2,21 @@ import axios from "axios"
 import { createSearchParams } from "react-router-dom"
 
 // 상품상세페이지 리뷰 목록
-export const getProductReviewList = async(rno,page) => {
+export const getProductReviewList = async(pno,page) => {
     
-    const res = await axios.get(`http://localhost:8080/api/review/list/product/${rno}?page=${page}&size=${6}`)
+    const res = await axios.get(`http://localhost:8080/api/review/list/product/${pno}?page=${page}&size=${6}`)
     return res.data
 
 }
+// 마이페이지 리뷰 목록
+export const getMyPageReviewList = async(queryObj, memberID) => {
 
+    const queryString = createSearchParams(queryObj).toString()
+    const res = await axios.get(`http://localhost:8080/api/review/list/mypage/${memberID}?${queryString}`)
+    return res.data
+
+}
+// 리뷰 작성
 export const registReview = async(reviewInfo) => {
 
     const res = await axios.post(`http://localhost:8080/api/review/regist`,reviewInfo)
--- src/components/products/ListComponent.js
+++ src/components/products/ListComponent.js
@@ -17,8 +17,7 @@ const initState = {
 };
 
 const ListComponent = ({ queryObj, movePage, moveRead }) => {
-  
-  const [productList, setProductList] = useState({...initState});
+  const [productList, setProductList] = useState({ ...initState });
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -31,50 +30,55 @@ const ListComponent = ({ queryObj, movePage, moveRead }) => {
 
   return (
     <div>
-<div className="container mx-auto my-5">
-  <h1 className="text-2xl font-bold mb-3">Product List</h1>
-  <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-    {productList.list.map(
-      ({ pno, productName, productPrice, imgName, categoryName }) => (
-        <div
-          key={pno}
-          className="p-4 border rounded-md hover:shadow-lg transition duration-300"
-          onClick={() => moveRead(pno)}
-        >
-          <div className="flex">
-            <div className="w-20 h-20 overflow-hidden rounded-full">
-              <img
-                src={`http://localhost/product/${imgName}`}
-                className="w-full h-full object-cover"
-                alt={productName}
-              />
-            </div>
-            <div className="flex flex-col ml-2">
-              <div className="text-lg font-semibold mb-1">{productName}</div>
-              <div className="text-gray-500">
-                category : {categoryName}
+      <div className="container mx-auto my-5">
+        <h1 className="text-2xl font-bold mb-3">Product List</h1>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          {productList.list.map(
+            ({ pno, productName, productPrice, imgName, categoryName }) => (
+              <div
+                key={pno}
+                className="p-4 border rounded-md hover:shadow-lg transition duration-300"
+                onClick={() => moveRead(pno)}
+              >
+                <div className="flex">
+                  <div className="w-20 h-20 overflow-hidden rounded-full">
+                    <img
+                      src={`http://localhost/product/${imgName}`}
+                      className="w-full h-full object-cover"
+                      alt={productName}
+                    />
+                  </div>
+                  <div className="flex flex-col ml-2">
+                    <div className="text-lg font-semibold mb-1">
+                      {productName}
+                    </div>
+                    <div className="text-gray-500">
+                      category : {categoryName}
+                    </div>
+                    <div className="text-gray-500">
+                      Price: {productPrice.toLocaleString()}원
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="text-gray-500">
-                Price: {productPrice.toLocaleString()}원
-              </div>
-            </div>
-          </div>
+            )
+          )}
         </div>
-      )
-    )}
-  </div>
-</div>
-<div className="float-right top-4 right-4">
-  <button
-    className="bg-[#dc4a51] text-white mr-5 px-4 py-2 rounded-md mt-5"
-    onClick={() => navigate("../regist")}
-  >
-    상품 등록
-  </button>
-</div>
-<div>
-  <ListPageComponent movePage={movePage} {...productList}></ListPageComponent>
-</div>
+      </div>
+      <div className="float-right top-4 right-4">
+        <button
+          className="bg-[#dc4a51] text-white mr-5 px-4 py-2 rounded-md mt-5"
+          onClick={() => navigate("../regist")}
+        >
+          상품 등록
+        </button>
+      </div>
+      <div>
+        <ListPageComponent
+          movePage={movePage}
+          {...productList}
+        ></ListPageComponent>
+      </div>
     </div>
   );
 };
