0daa4216b7b3e49c439f552b3cf172e186dcc25c
JY LEE <wndyd0110@gmail.com>
2024-03-22 20:37:29 +0900

    리뷰답글 구현 완료

--- src/pages/review/ReadPage.js
+++ src/pages/review/ReadPage.js
@@ -1,6 +1,17 @@
+import { useParams } from "react-router-dom";
+import useQueryObj from "../../hooks/useQueryObj";
+import ReadComponent from "../../components/review/ReadComponent";
+
 const ReadPage = () => {
-    return ( 
-        <div>ReadReview</div>
+
+    const{queryObj, moveModify ,moveList} = useQueryObj();
+
+    const {rno} = useParams();
+
+    return (
+        <div>
+            <ReadComponent rno ={rno} queryObj={queryObj} moveModify = {moveModify} moveList={moveList}></ReadComponent>
+        </div>
      );
 }
  
